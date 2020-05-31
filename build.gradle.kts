c7fcc0468694a0038d08e215e703fd91efe3a533
Isak W <isak.wertwein@gmail.com>
2020-05-31 13:42:59 +0200

    Initial commit

---
+++ build.gradle.kts
@@ -0,0 +1,34 @@
+import org.jetbrains.kotlin.gradle.tasks.*
+
+plugins {
+    kotlin("jvm") version "1.3.61"
+    id("org.jetbrains.intellij") version "0.4.21"
+}
+
+group = "io.aesy.yamllint"
+version = "0.1"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation(kotlin("stdlib-jdk8"))
+}
+
+intellij {
+    version = "IC-2020.1"
+    pluginName = rootProject.name
+    updateSinceUntilBuild = false
+}
+
+tasks.withType<KotlinCompile> {
+    sourceCompatibility = "1.8"
+    targetCompatibility = "1.8"
+
+    kotlinOptions {
+        jvmTarget = "1.8"
+        apiVersion = "1.3"
+        languageVersion = "1.3"
+    }
+}
---
+++ settings.gradle.kts
@@ -0,0 +1,1 @@
+rootProject.name = "yamllint-intellij"
