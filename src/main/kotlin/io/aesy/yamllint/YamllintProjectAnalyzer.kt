bacf6b236a4bddf6f838f918a297d4ac54cf7b13
Isak W <isak.wertwein@gmail.com>
2020-11-08 13:26:51 +0100

    Add settings state

---
+++ src/main/kotlin/io/aesy/yamllint/YamllintProjectAnalyzer.kt
@@ -0,0 +1,60 @@
+package io.aesy.yamllint
+
+import com.intellij.openapi.application.ReadAction
+import com.intellij.openapi.components.Service
+import com.intellij.openapi.project.Project
+import com.intellij.openapi.vfs.VirtualFile
+
+@Service
+class YamllintProjectAnalyzer(
+    project: Project
+) {
+    companion object {
+        private val logger = getLogger()
+        private const val DEFAULT_BIN_PATH = "yamllint"
+        private const val DEFAULT_CONFIG_PATH = ""
+    }
+
+    private val executableFinder = project.getService<YamllintExecutableFinder>()
+    private val configurationFinder = project.getService<YamllintConfigurationFinder>()
+
+    private var settings: YamllintSettings? = null
+
+    @Synchronized
+    fun getSuggestedSettings(): YamllintSettings {
+        if (settings != null) {
+            return settings!!
+        }
+
+        logger.debug("Analyzing project...")
+
+        logger.debug("Searching for yamllint executable...")
+        val executable = executableFinder.find()
+        val binPath = executable?.path ?: DEFAULT_BIN_PATH
+
+        if (executable == null) {
+            logger.info("No yamllint executable found, defaulting to '${DEFAULT_BIN_PATH}'")
+        } else {
+            logger.info("Found yaml executable at '$binPath'")
+        }
+
+        logger.debug("Searching for yamllint configuration...")
+        val configuration = ReadAction.compute<VirtualFile, Throwable> { configurationFinder.find()?.virtualFile }
+        val configPath = configuration?.path ?: DEFAULT_CONFIG_PATH
+
+        if (configuration == null) {
+            logger.info("No yamllint configuration found, defaulting to '${DEFAULT_CONFIG_PATH}'")
+        } else {
+            logger.info("Found yamllint configuration at '$configPath'")
+        }
+
+        val enabled = !configuration?.path.isNullOrBlank()
+
+        settings = YamllintSettings(
+            enabled = enabled,
+            binPath = binPath,
+            configPath = configPath)
+
+        return settings!!
+    }
+}
---
+++ src/main/kotlin/io/aesy/yamllint/YamllintSettingsProvider.kt
@@ -0,0 +1,24 @@
+package io.aesy.yamllint
+
+import com.intellij.openapi.components.*
+import com.intellij.openapi.project.Project
+
+@Service
+@State(name = "YamllintSettings", storages = [Storage(StoragePathMacros.WORKSPACE_FILE)])
+class YamllintSettingsProvider(
+    project: Project
+) : PersistentStateComponent<YamllintSettings> {
+    private val analyzer = project.getService<YamllintProjectAnalyzer>()
+
+    private lateinit var settings: YamllintSettings
+
+    override fun getState(): YamllintSettings = settings
+
+    override fun loadState(settings: YamllintSettings) {
+        this.settings = settings
+    }
+
+    override fun noStateLoaded() {
+        this.settings = analyzer.getSuggestedSettings()
+    }
+}
